b5c6b67d3553337f4a924ce61d2739395102d846
mnklop <maxim210204@yandex.ru>
2020-10-22 15:55:56 +0300

    Create ft_remove_str.py

---
+++ ft_remove_str.py
@@ -0,0 +1,15 @@
+from . import ft_len as l
+
+
+def ft_remove_str(str1, str2):
+    sc = 0
+    otv = ''
+    for i in str1:
+        if i in str2:
+            sc += 1
+        else:
+            otv += i
+    if l.ft_len(str1) == sc:
+        return False
+    else:
+        return otv
