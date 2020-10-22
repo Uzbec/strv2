96178fe6fcc3d734ab956dfefac8c364ab3d026f
mnklop <maxim210204@yandex.ru>
2020-10-22 12:24:47 +0300

    Update ft_analysis.py

--- ft_analysis.py
+++ ft_analysis.py
@@ -1,27 +1,23 @@
-def ft_len(str):
-    a = 0
-    for i in str:
-        a += 1
-    return a
+from . import ft_len as l
 
 
 def ft_analysis(str):
     print(str[2])
     print(str[-2])
     print(str[0] + str[1] + str[2] + str[3] + str[4])
-    for i in range(ft_len(str) - 2):
+    for i in range(l.ft_len(str) - 2):
         print(str[i], end="")
     print(end="\n")
-    for i in range(0, ft_len(str), 2):
+    for i in range(0, l.ft_len(str), 2):
         print(str[i], end="")
     print(end="\n")
-    for i in range(1, ft_len(str), 2):
+    for i in range(1, l.ft_len(str), 2):
         print(str[i], end="")
     print(end="\n")
-    for i in range(ft_len(str) - 1, -1, -1):
+    for i in range(l.ft_len(str) - 1, -1, -1):
         print(str[i], end="")
     print(end="\n")
-    for i in range(ft_len(str) - 1, -1, -2):
+    for i in range(l.ft_len(str) - 1, -1, -2):
         print(str[i], end="")
     print(end="\n")
-    print(ft_len(str))
+    print(l.ft_len(str))
