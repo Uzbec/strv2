683fcb2a36241274d0ed610652fcf45f7e59d00f
mnklop <maxim210204@yandex.ru>
2020-11-04 21:00:19 +0300

    1

--- ft_same_parts_list.py
+++ ft_same_parts_list.py
@@ -3,7 +3,7 @@ from . import ft_len as l
 
 def ft_same_parts_list(mass):
     for i in range(1, l.ft_len(mass)):
-        if (mass[i - 1] > 0 and mass[i] > 0) or (mass[i - 1] < 0 and mass[i]
-                                                 < 0):
+        if (mass[i - 1] > 0 and mass[i] > 0) or (
+                mass[i - 1] < 0 and mass[i] < 0):
             return True
     return False
