342ada1d20ba4a7c224b657e16302e3cd82f9ecc
mnklop <44546223+mnklop@users.noreply.github.com>
2020-11-12 00:00:22 +0300

    Update ft_join.py

--- ft_join.py
+++ ft_join.py
@@ -1,10 +1,12 @@
 from . import ft_len as l
 
 
-def ft_join(lst, sep):
+def ft_join(lst, *args):
     sp = ""
-    if not sep:
+    if not args:
         sep = " "
+    else:
+        sep = args[0]
     for i in range(l.ft_len(lst) - 1):
         sp += (lst[i] + sep)
     sp += lst[-1]
