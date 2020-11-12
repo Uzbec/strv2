42417df0e6e232318eb56d4d75a60ce57d88a81a
mnklop <maxim210204@yandex.ru>
2020-11-12 11:13:26 +0300

    1

--- ft_odd_even_analysis_lst.py
+++ ft_odd_even_analysis_lst.py
@@ -1,11 +1,11 @@
-from . import ft_odd_even_separator_lst as k
-from . import ft_max_min_sum_count as mms
+import ft_odd_even_separator_lst as k
+import ft_max_min_sum_count as mms
 
 
 def ft_odd_even_analysis_lst(lst):
     ch = k.ft_odd_even_separator_lst(lst)[0]
     nech = k.ft_odd_even_separator_lst(lst)[1]
-    kch = mms.ft_max_min_sum_count(ch)[0]
+    s = mms.ft_max_min_sum_count(ch)[0]
     knech = mms.ft_max_min_sum_count(nech)[0]
     mch = mms.ft_max_min_sum_count(ch)[1]
     mnech = mms.ft_max_min_sum_count(nech)[1]
@@ -13,8 +13,9 @@ def ft_odd_even_analysis_lst(lst):
     minech = mms.ft_max_min_sum_count(nech)[2]
     sch = mms.ft_max_min_sum_count(ch)[3]
     snch = mms.ft_max_min_sum_count(nech)[3]
-    otv = f"Анализ списка:\nКоличество четных чисел: {kch},\t\t" \
-          f"Количество нечетных чисел: {knech}\nМаксимальная четная " \
+    otv = f"Анализ списка:\nКоличество четных чисел: {s},\t\t" \
+          f"Количество нечетных чисел: {knech}" \
+          f"\nМаксимальная четная " \
           f"цифра: {mch},\t\tМаксимальная нечетная цифра: {mnech}," \
           f"\nМинимальная четная цифра: {mich},\t\tМинимальная нечетная " \
           f"цифра: {minech},\nСумма четных чисел: {sch},\t\tСумма нечетных " \
